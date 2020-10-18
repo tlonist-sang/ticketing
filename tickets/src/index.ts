6571a67568e363bdeba34ec72e704589db10339e
tlonist.sang <tlonist.sang@gmail.com>
2020-10-18 19:45:49 +0900

    update

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,6 +1,7 @@
 import 'express-async-errors';
 import mongoose from 'mongoose';
 import {app} from "./app";
+import {natsWrapper} from "./nats-wrapper";
 
 const start = async () => {
     if(!process.env.JWT_KEY) {
@@ -12,6 +13,7 @@ const start = async () => {
     }
 
     try {
+        await natsWrapper.connect('ticketing', 'randome', 'http://nats-srv:4222');
         await mongoose.connect(process.env.MONGO_URI), {
             useNewUrlParser: true,
             useUnifiedTopology: true,
